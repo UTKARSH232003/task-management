c28098a04a5970a9f3838f044205b53b3698021a
Utkarsh <utkarshsingh6207603306@gmail.com>
2024-06-30 12:37:11 +0530

    again change in api

--- frontend/src/app/task.service.ts
+++ frontend/src/app/task.service.ts
@@ -8,9 +8,9 @@ import { Observable } from 'rxjs';
 export class TaskService {
   // private apiUrl = 'http://localhost:5001/tasks/';
   // private statusApiUrl = 'http://localhost:5001/status/change-status/';
-  private apiUrl = 'http://task-management-8ied.onrender.com/tasks/';
+  private apiUrl = 'http://task-management-1-zwte.onrender.com/tasks/';
 
-  private statusApiUrl = 'http://task-management-8ied.onrender.com/status/change-status/';
+  private statusApiUrl = 'http://task-management-1-zwte.onrender.com/status/change-status/';
 
   
   constructor(private http: HttpClient) { }
